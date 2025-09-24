b736b03a0b3472f374957ea3e8877b274d650e2e
imrrobat <imrrobat@gmail.com>
2025-09-24 20:54:16 +0330

    state adedd

--- app.py
+++ app.py
@@ -183,9 +183,30 @@ def today():
     for item in log:
         count_smile += int(item['priority'])
     
+    yesterday = today - jdt.timedelta(days=1)
+    yesterday_str = f'{yesterday.year}-{yesterday.month:02d}-{yesterday.day:02d}'
+    
+    yesterday_log = today_log(yesterday_str)
+    count_smile_yesterday = 0
+    
+    for item in yesterday_log:
+        count_smile_yesterday += int(item['priority'])
+    
+    change_percent = (count_smile - count_smile_yesterday) / count_smile_yesterday * 100
+    
+
+    
     with ui.row():
         ui.label(f'گزارش امروز {today.year}/{today.month}/{today.day}').style('font-weight:bold;')
         ui.label(f'لبخندهای کسب شده: {count_smile}').style('font-weight:bold;')
+        
+        if change_percent > 0:
+            ui.label(f"امروز نسبت به دیروز {change_percent:.1f}% بهتر بودی").style('font-weight:bold; color:#08CB00;')
+        elif change_percent < 0:
+            ui.label(f"امروز نسبت به دیروز {abs(change_percent):.1f}% بدتر بودی").style('font-weight:bold; color:#E62727;')
+        else:
+            ui.label("هیچ تغییری نسبت به دیروز نداشتی").style('font-weight:bold;')
+    
     with ui.row().style('width: 100%; gap: 8px;'):
         with ui.column().style('flex: 1; border: 1px solid #ccc; padding: 8px;'):
             work_today = [item for item in log if item['category']=='work']
@@ -240,6 +261,22 @@ def stat():
     toggle_btn = ui.button(icon='bedtime', on_click=toggle_dark).classes(
         'fixed bottom-4 left-4 text-white'
     ).props('color=purple')
+
+    ui.markdown('###آمار 10 روز قبل')
+    
+    today = jdt.datetime.now()
+
+    last_10_days = []
+    
+    for i in range(10):
+        day = today - jdt.timedelta(days=i)
+        day_str = f'{day.year}-{day.month:02d}-{day.day:02d}'
+        last_10_days.append(day_str)
+    
+    for day in last_10_days:
+        day_log = today_log(day)
+        smile_count = sum(int(item['priority']) for item in day_log)
+        ui.label(f'روز {day} - لبخندها {smile_count}')
 
     
     ui.link('بازگشت به صفحه اصلی','http://127.0.0.1:8083/').classes(
